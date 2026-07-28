82cc49a69fcc71551639b307556e08bd12b96b32
Muhammad Sheheryar <79762785+sherikhan452@users.noreply.github.com>
2026-07-29 01:41:15 +0500

    Starting Function

---
+++ index.php
@@ -0,0 +1,16 @@
+
+
+<?php 
+
+function Myfunction($name){
+
+    echo "My name is: $name</br>";
+
+}
+
+myfunction("SK Digital");
+myfunction("Sheri");
+
+
+?>
+<h1> <?php echo bloginfo('name'); ?></h1>
