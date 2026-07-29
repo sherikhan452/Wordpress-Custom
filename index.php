159fa69ca16e94d0e8319c9592dbcd43bdfc0de8
Muhammad Sheheryar <79762785+sherikhan452@users.noreply.github.com>
2026-07-29 20:40:45 +0500

    Permalink

--- index.php
+++ index.php
@@ -5,8 +5,8 @@
 while(have_posts()){
     the_post();
     ?>
-    <h1><?php the_title(); ?> </h1>
+    <h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a> </h1>
     <?php the_content(); ?>
-<?php }?>
+<?php } ?>
 
 
