37d36b75be0b15ca6864ca6abc06405d4477d184
Muhammad Sheheryar <79762785+sherikhan452@users.noreply.github.com>
2026-07-30 20:16:03 +0500

    Implement Template

--- functions.php
+++ functions.php
@@ -1,7 +1,11 @@
 <?php
 
 function university_files() {
-    wp_enqueue_style('university_main_styles', get_stylesheet_uri());
+    
+    wp_enqueue_style('university_main_styles', get_theme_file_uri('/build/style-index.css'));
+    wp_enqueue_style('university_main_styles', get_theme_file_uri('/build/style-index.css'));
+    wp_enqueue_style('font-Awesome-Icon','//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
+    wp_enqueue_style('google-fonts','//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
 }
 
 add_action('wp_enqueue_scripts', 'university_files');
