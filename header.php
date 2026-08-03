581bc3b3bb1be054190862e486ea07387d12190a
Muhammad Sheheryar <79762785+sherikhan452@users.noreply.github.com>
2026-08-03 23:15:53 +0500

    Class Change Navigation

--- header.php
+++ header.php
@@ -18,23 +18,17 @@
           <nav class="main-navigation">
    
    
-        <?php
-
-            wp_nav_menu(array(
-              'theme_location' => 'TopHeaderMenuLocation'
-            ));
-
-          ?>
+       
    
-          <!-- <ul>
-              <li><a href="<?php echo site_url('/about-us'); ?>">About Us</a></li>
+          <ul>
+              <li <?php if(is_page('about-us') or wp_get_post_parent_id(0) == 17) echo 'class="current-menu-item"' ?>><a href="<?php echo site_url('/about-us'); ?>">About Us</a></li>
               <li><a href="#">Programs</a></li>
               <li><a href="#">Events</a></li>
               <li><a href="#">Campuses</a></li>
               <li><a href="#">Blog</a></li>
             </ul>
 
--->
+
           </nav>
           <div class="site-header__util">
             <a href="#" class="btn btn--small btn--orange float-left push-right">Login</a>
