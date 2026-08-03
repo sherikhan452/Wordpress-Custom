da95c85b1a1da99c37f8db19bad2e475fb094a84
Muhammad Sheheryar <79762785+sherikhan452@users.noreply.github.com>
2026-08-03 18:29:08 +0500

    BreakCrumps

--- page.php
+++ page.php
@@ -34,18 +34,43 @@ while(have_posts()){
         
       
 
+    ?>  
+
+
+    
+    <?php 
+
+    $fullPagesList = get_pages(array(
+
+    'child_of'=> get_the_ID(),
+
+    ));
+
+    if($x or $fullPagesList){ ?>
+
+    <div class="page-links">
+
+    <h2 class="page-links__title"><a href="<?php echo get_permalink($x);?>"><?php echo get_the_title($x); ?></a></h2>
+    <ul class="min-list">
+    <?php
+
+    if($x){
+    $childrenof=$x;
+    }
+    else{
+    $childrenof = get_the_ID();
+    }
+    wp_list_pages(array(
+    'title_li' => Null,
+    'child_of' => $childrenof
+    ));
+
     ?>
-    
-    
-     <!-- 
-      <div class="page-links">
-        <h2 class="page-links__title"><a href="#">About Us</a></h2>
-        <ul class="min-list">
-          <li class="current_page_item"><a href="#">Our History</a></li>
-          <li><a href="#">Our Goals</a></li>
-        </ul>
-      </div>
--->
+    </ul>
+    </div>
+    <?php } ?>
+     
+     
       <div class="generic-content">
         <?php the_content(); ?>
       </div>
