b4faa56abd93c31d95f94208167204f6e9ef3136
Muhammad Sheheryar <79762785+sherikhan452@users.noreply.github.com>
2026-08-03 23:06:08 +0500

    Menus Added

--- functions.php
+++ functions.php
@@ -14,7 +14,9 @@ add_action('wp_enqueue_scripts', 'university_files');
 
 
 function university_feature(){
-
+    register_nav_menu('TopHeaderMenuLocation','Header Menu Location');
+    register_nav_menu('footerLocationOne','Footer Location One');
+    register_nav_menu('footerLocationTwo','Footer Location Two');
     add_theme_support('title-tag');
 
 }
--- header.php
+++ header.php
@@ -6,7 +6,7 @@
     
 </head>
 
-<body>
+<body <?php body_class(); ?>>
     <header class="site-header">
       <div class="container">
         <h1 class="school-logo-text float-left">
@@ -16,13 +16,25 @@
         <i class="site-header__menu-trigger fa fa-bars" aria-hidden="true"></i>
         <div class="site-header__menu group">
           <nav class="main-navigation">
-            <ul>
+   
+   
+        <?php
+
+            wp_nav_menu(array(
+              'theme_location' => 'TopHeaderMenuLocation'
+            ));
+
+          ?>
+   
+          <!-- <ul>
               <li><a href="<?php echo site_url('/about-us'); ?>">About Us</a></li>
               <li><a href="#">Programs</a></li>
               <li><a href="#">Events</a></li>
               <li><a href="#">Campuses</a></li>
               <li><a href="#">Blog</a></li>
             </ul>
+
+-->
           </nav>
           <div class="site-header__util">
             <a href="#" class="btn btn--small btn--orange float-left push-right">Login</a>
